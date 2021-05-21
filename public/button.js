4863b1a09ac9ea872fe152a9e0d355e854cfbb6f
quanghieu265 <hieu26594@gmail.com>
2021-05-21 22:57:54 +0700

    test2

--- public/button.js
+++ public/button.js
@@ -9,10 +9,12 @@ $('.close').click(function() {
     $('.modal').toggleClass('fade-in');
 })
 $('.accept').click(function() {
+    //xóa thông tin user trên file database, dựa theo ID đã lấy khi click vào nút Xóa
     $.ajax({
         url: `https://list-techmaster123.herokuapp.com/users/${userID}`,
         type: 'DELETE',
     });
+    // xóa div chứa thông tin user trên HTML 
     $('.info-list').eq(index).remove();
     $('.modal-show').toggleClass('show');
     $('.modal').toggleClass('fade-in');
--- public/database.js
+++ public/database.js
@@ -9,15 +9,12 @@ xmlhttp.onreadystatechange = function() {
         for (i = 0; i < myObj.users.length; i++) {
             // <!-- $('.cloneitem:first').clone().appendTo('#container'); -->
             // <!-- Mỗi vằng lặp i sẽ clone lại rồi fill giá trị -->
+            myObj.users[i].name == "" ? document.getElementsByClassName("name")[i].innerHTML = "chưa biết" : document.getElementsByClassName("name")[i].innerHTML = myObj.users[i].name
+            myObj.users[i].birthday == "" ? document.getElementsByClassName("birth")[i].innerHTML = "chưa biết" : document.getElementsByClassName("birth")[i].innerHTML = myObj.users[i].birthday
+            myObj.users[i].email == "" ? document.getElementsByClassName("email")[i].innerHTML = "chưa biết" : document.getElementsByClassName("email")[i].innerHTML = myObj.users[i].email
+            myObj.users[i].phone == "" ? document.getElementsByClassName("number")[i].innerHTML = "chưa biết" : document.getElementsByClassName("number")[i].innerHTML = myObj.users[i].phone
 
-            document.getElementsByClassName("name")[i].innerHTML = myObj.users[i].name;
-
-            if (myObj.users[i].birthday == "") {
-                document.getElementsByClassName("birth")[i].innerHTML = "chưa biết";
-            } else { document.getElementsByClassName("birth")[i].innerHTML = myObj.users[i].birthday; }
-            document.getElementsByClassName("email")[i].innerHTML = myObj.users[i].email;
-            document.getElementsByClassName("number")[i].innerHTML = myObj.users[i].phone;
-
+            // điều kiện để không clone ở cuôi vòng lặp
             if (i < myObj.users.length - 1) {
                 $(".info-list:first").clone().appendTo(".student-list");
             }
@@ -25,6 +22,7 @@ xmlhttp.onreadystatechange = function() {
         $('.remove').click(function() {
             $('.modal-show').toggleClass('show');
             $('.modal').toggleClass('fade-in');
+            // Tìm số ID của User khi click vào nút Xóa, bằng cách tìm theo tên User
             index = $('.remove').index(this)
             nameFinder = $('.name').eq(index).html()
             for (i = 0; i < myObj.users.length; i++) {
@@ -34,7 +32,7 @@ xmlhttp.onreadystatechange = function() {
             }
         })
         $(".edit").click(function() {
-
+            // Tìm số ID của User khi click vào nút Chỉnh Sửa, bằng cách tìm theo tên User
             index = $('.edit').index(this)
             nameFinder = $('.name').eq(index).html()
             for (i = 0; i < myObj.users.length; i++) {
@@ -42,9 +40,10 @@ xmlhttp.onreadystatechange = function() {
                     userID = myObj.users[i].id;
                 }
             }
+            // Đưa số ID vào file userIdStorage trong database để sử dụng ở các file HTML khác
             $.ajax({
                 type: "PUT",
-                url: "https://list-techmaster123.herokuapp.com/userIndex/1",
+                url: "https://list-techmaster123.herokuapp.com/userIdStorage/1",
                 data: ` {
                     "UI":${userID}
                    }`,
