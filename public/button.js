98103a1eb4ef03b7dae911ac95ce487a4ac28d62
quanghieu265 <hieu26594@gmail.com>
2021-05-26 14:25:23 +0700

    aaaa

--- public/button.js
+++ public/button.js
@@ -1,21 +1,30 @@
+// Close Modal khi ấn ra ngoài modal
 $(window).click(function(e) {
-    if ($(e.target).is(".modal-show")) {
-        $('.modal-show').toggleClass('show');
-        $('.modal').toggleClass('fade-in');
-    }
-})
+        if ($(e.target).is(".modal-show")) {
+            $('.modal-show').toggleClass('show');
+            $('.modal').toggleClass('fade-in');
+        }
+    })
+    // close modal Khi ấn vào nút " Hủy "
 $('.close').click(function() {
     $('.modal-show').toggleClass('show');
     $('.modal').toggleClass('fade-in');
 })
+
+// Function xóa dữ liệu khi ấn nút "đồng ý" ở modal
 $('.accept').click(function() {
     //xóa thông tin user trên file database, dựa theo ID đã lấy khi click vào nút Xóa
     $.ajax({
         url: `https://list-techmaster123.herokuapp.com/users/${userID}`,
         type: 'DELETE',
+    }).done(function() {
+        // xóa div chứa thông tin user trên HTML 
+        $('.info-list').eq(index).remove();
+    }).fail(function() {
+        alert("Có lỗi xảy ra, không thể xóa dữ liệu.")
     });
-    // xóa div chứa thông tin user trên HTML 
-    $('.info-list').eq(index).remove();
+
+
     $('.modal-show').toggleClass('show');
     $('.modal').toggleClass('fade-in');
 })
--- public/database.js
+++ public/database.js
@@ -1,58 +1,112 @@
-let xmlhttp = new XMLHttpRequest; // tạo 1 biến chứa cái xml request để lấy data từ sever
 let myObj;
 let userID;
-//Xác định một hàm được gọi khi thuộc tính readyState thay đổi 
-xmlhttp.onreadystatechange = function() {
+// let xmlhttp = new XMLHttpRequest; // tạo 1 biến chứa cái xml request để lấy data từ sever
+// // Xác định một hàm được gọi khi thuộc tính readyState thay đổi 
+// xmlhttp.onreadystatechange = function() {
 
-    if (this.readyState == 4 && this.status == 200) {
-        myObj = JSON.parse(this.responseText);
+//     if (this.readyState == 4 && this.status == 200) {
+//         myObj = JSON.parse(this.responseText);
+//         for (i = 0; i < myObj.users.length; i++) {
+//             // <!-- $('.cloneitem:first').clone().appendTo('#container'); -->
+//             // <!-- Mỗi vằng lặp i sẽ clone lại rồi fill giá trị -->
+//             myObj.users[i].name == "" ? document.getElementsByClassName("name")[i].innerHTML = "chưa biết" : document.getElementsByClassName("name")[i].innerHTML = myObj.users[i].name
+//             myObj.users[i].birthday == "" ? document.getElementsByClassName("birth")[i].innerHTML = "chưa biết" : document.getElementsByClassName("birth")[i].innerHTML = myObj.users[i].birthday
+//             myObj.users[i].email == "" ? document.getElementsByClassName("email")[i].innerHTML = "chưa biết" : document.getElementsByClassName("email")[i].innerHTML = myObj.users[i].email
+//             myObj.users[i].phone == "" ? document.getElementsByClassName("number")[i].innerHTML = "chưa biết" : document.getElementsByClassName("number")[i].innerHTML = myObj.users[i].phone
+
+//             // điều kiện để không clone ở cuôi vòng lặp
+//             if (i < myObj.users.length - 1) {
+//                 $(".info-list:first").clone().appendTo(".student-list");
+//             }
+//         }
+//         $('.remove').click(function() {
+//             $('.modal-show').toggleClass('show');
+//             $('.modal').toggleClass('fade-in');
+//             // Tìm số ID của User khi click vào nút Xóa, bằng cách tìm theo tên User
+//             index = $('.remove').index(this)
+//             nameFinder = $('.name').eq(index).html()
+//             for (i = 0; i < myObj.users.length; i++) {
+//                 if (nameFinder === myObj.users[i].name) {
+//                     userID = myObj.users[i].id;
+//                 }
+//             }
+//         })
+//         $(".edit").click(function() {
+//             // Tìm số ID của User khi click vào nút Chỉnh Sửa, bằng cách tìm theo tên User
+//             index = $('.edit').index(this)
+//             nameFinder = $('.name').eq(index).html()
+//             for (i = 0; i < myObj.users.length; i++) {
+//                 if (nameFinder === myObj.users[i].name) {
+//                     userID = myObj.users[i].id;
+//                 }
+//             }
+//             // Đưa số ID vào file userIdStorage trong database để sử dụng ở các file HTML khác
+//             $.ajax({
+//                 type: "PUT",
+//                 url: "https://list-techmaster123.herokuapp.com/userIdStorage/1",
+//                 data: ` {
+//                     "UI":${userID}
+//                    }`,
+//                 contentType: "application/json",
+//                 dataType: 'json'
+//             });
+//         })
+//     }
+// };
+// //Bắt đầu lấy dữ liệu từ sever
+// xmlhttp.open("GET", "https://list-techmaster123.herokuapp.com/db", true);
+// xmlhttp.send();
+
+$.ajax({
+    type: "GET",
+    url: "https://list-techmaster123.herokuapp.com/db"
+}).done(function(responseText) {
+    myObj = responseText;
+    for (i = 0; i < myObj.users.length; i++) {
+        // <!-- $('.cloneitem:first').clone().appendTo('#container'); -->
+        // <!-- Mỗi vằng lặp i sẽ clone lại rồi fill giá trị -->
+        myObj.users[i].name == "" ? document.getElementsByClassName("name")[i].innerHTML = "chưa biết" : document.getElementsByClassName("name")[i].innerHTML = myObj.users[i].name
+        myObj.users[i].birthday == "" ? document.getElementsByClassName("birth")[i].innerHTML = "chưa biết" : document.getElementsByClassName("birth")[i].innerHTML = myObj.users[i].birthday
+        myObj.users[i].email == "" ? document.getElementsByClassName("email")[i].innerHTML = "chưa biết" : document.getElementsByClassName("email")[i].innerHTML = myObj.users[i].email
+        myObj.users[i].phone == "" ? document.getElementsByClassName("number")[i].innerHTML = "chưa biết" : document.getElementsByClassName("number")[i].innerHTML = myObj.users[i].phone
+
+        // điều kiện để không clone ở cuôi vòng lặp
+        if (i < myObj.users.length - 1) {
+            $(".info-list:first").clone().appendTo(".student-list");
+        }
+    }
+
+    $('.remove').click(function() {
+        $('.modal-show').toggleClass('show');
+        $('.modal').toggleClass('fade-in');
+        // Tìm số ID của User khi click vào nút Xóa, bằng cách tìm theo tên User
+        index = $('.remove').index(this)
+        nameFinder = $('.name').eq(index).html()
         for (i = 0; i < myObj.users.length; i++) {
-            // <!-- $('.cloneitem:first').clone().appendTo('#container'); -->
-            // <!-- Mỗi vằng lặp i sẽ clone lại rồi fill giá trị -->
-            myObj.users[i].name == "" ? document.getElementsByClassName("name")[i].innerHTML = "chưa biết" : document.getElementsByClassName("name")[i].innerHTML = myObj.users[i].name
-            myObj.users[i].birthday == "" ? document.getElementsByClassName("birth")[i].innerHTML = "chưa biết" : document.getElementsByClassName("birth")[i].innerHTML = myObj.users[i].birthday
-            myObj.users[i].email == "" ? document.getElementsByClassName("email")[i].innerHTML = "chưa biết" : document.getElementsByClassName("email")[i].innerHTML = myObj.users[i].email
-            myObj.users[i].phone == "" ? document.getElementsByClassName("number")[i].innerHTML = "chưa biết" : document.getElementsByClassName("number")[i].innerHTML = myObj.users[i].phone
-
-            // điều kiện để không clone ở cuôi vòng lặp
-            if (i < myObj.users.length - 1) {
-                $(".info-list:first").clone().appendTo(".student-list");
+            if (nameFinder === myObj.users[i].name) {
+                userID = myObj.users[i].id;
             }
         }
-        $('.remove').click(function() {
-            $('.modal-show').toggleClass('show');
-            $('.modal').toggleClass('fade-in');
-            // Tìm số ID của User khi click vào nút Xóa, bằng cách tìm theo tên User
-            index = $('.remove').index(this)
-            nameFinder = $('.name').eq(index).html()
-            for (i = 0; i < myObj.users.length; i++) {
-                if (nameFinder === myObj.users[i].name) {
-                    userID = myObj.users[i].id;
-                }
+    })
+
+    $(".edit").click(function() {
+        // Tìm số ID của User khi click vào nút Chỉnh Sửa, bằng cách tìm theo tên User
+        index = $('.edit').index(this)
+        nameFinder = $('.name').eq(index).html()
+        for (i = 0; i < myObj.users.length; i++) {
+            if (nameFinder === myObj.users[i].name) {
+                userID = myObj.users[i].id;
             }
-        })
-        $(".edit").click(function() {
-            // Tìm số ID của User khi click vào nút Chỉnh Sửa, bằng cách tìm theo tên User
-            index = $('.edit').index(this)
-            nameFinder = $('.name').eq(index).html()
-            for (i = 0; i < myObj.users.length; i++) {
-                if (nameFinder === myObj.users[i].name) {
-                    userID = myObj.users[i].id;
-                }
-            }
-            // Đưa số ID vào file userIdStorage trong database để sử dụng ở các file HTML khác
-            $.ajax({
-                type: "PUT",
-                url: "https://list-techmaster123.herokuapp.com/userIdStorage/1",
-                data: ` {
-                    "UI":${userID}
-                   }`,
-                contentType: "application/json",
-                dataType: 'json'
-            });
-        })
-    }
-};
-//Bắt đầu lấy dữ liệu từ sever
-xmlhttp.open("GET", "https://list-techmaster123.herokuapp.com/db", true);
-xmlhttp.send();
+        }
+        // Đưa số ID vào file userIdStorage trong database để sử dụng ở các file HTML khác
+        $.ajax({
+            type: "PUT",
+            url: "https://list-techmaster123.herokuapp.com/userIdStorage/1",
+            data: ` {
+                "UI":${userID}
+               }`,
+            contentType: "application/json",
+            dataType: 'json'
+        });
+    })
+});
